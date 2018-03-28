b886768be63c26aae0a21bdfeed70d316dde56fb
Josephine Flood <jmgflood@gmail.com>
2018-03-28 12:13:07 +0100

    Add game timer

--- js/app.js
+++ js/app.js
@@ -11,7 +11,7 @@ const deck = document.querySelector('.deck');
  *   - add each card's HTML to the page
  */
 function makeCards() {
-	const shuffledCards = shuffle(icons); //insert shuffle function
+	const shuffledCards = icons; //insert shuffle function shuffle(icons)
 
 	for (let i = 0; i < shuffledCards.length; i++) {
 		const card = document.createElement("li");
@@ -19,9 +19,9 @@ function makeCards() {
 		const newCard = "<i class='fa fa-" + shuffledCards[i] + "'></i>";
 		card.innerHTML = newCard
 		deck.appendChild(card);
-		card.addEventListener("click", cardOpen);
 	}
-
+	deck.addEventListener("click", cardOpen);
+	deck.addEventListener("click", timer);
 }
 
 makeCards()
@@ -53,17 +53,20 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
 // matching cards logic
 let openCards = [];
 const cards = deck.getElementsByTagName('li');
 
-function cardOpen() {
- 	this.className = "card open show";
- 	openCards.push(this.innerHTML);
- 	if (openCards.length == 2) {
- 		compareCards();
- 	}
- 	moveCounter();
+function cardOpen(event) {
+	if (event.target.nodeName == "LI") {
+		event.target.className = "card open show";
+	 	openCards.push(event.target.innerHTML);
+	 	if (openCards.length == 2) {
+	 		compareCards();
+	 	}
+	 	moveCounter();
+	}
  }
 
 function compareCards() {
@@ -100,15 +103,13 @@ let clickCount = 0;
 
 function moveCounter() {
 	clickCount ++;
+	moves.innerHTML = clickCount;
 	 	if (clickCount == 15) {
 	 		stars[0].style.visibility = 'hidden';
-	 		moves.innerHTML = "2 moves";
 	 	} else if (clickCount == 25) {
 	 		stars[0, 1].style.visibility = 'hidden';
-	 		moves.innerHTML = "1 move";
 	 	} else if (clickCount == 40){
 	 		stars[0, 1, 2].style.visibility = 'hidden';
-	 		moves.innerHTML = "0 moves";
 	 		setTimeout(gameOver, 500);
 	 	}
 }
@@ -118,3 +119,29 @@ function gameOver() {
 }
 
 // timer
+const timerText = document.querySelector('.timer');
+let counter = 0;
+
+function timer(event) {
+	if (event.target.nodeName == "LI") {
+		deck.removeEventListener("click", timer);
+	}
+	timer = setInterval(function() {
+		const minutes = ((counter / 60) | 0) + "";
+		const seconds = (counter % 60) + "";
+		const format = ""
+                      + new Array(3-minutes.length).join("0") + minutes 
+                      + ':' 
+                      + new Array(3-seconds.length).join("0") + seconds;
+
+		timerText.innerHTML = format;
+		counter++;
+		// stop timer on game win
+		if (document.getElementsByClassName('card match').length == 16){
+				clearInterval(timer);
+
+		}
+
+	}, 1000);
+}
+
