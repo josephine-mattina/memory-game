ad8a766792b23320e904d1dcccd2f6d6d1f9c361
Josephine Flood <jmgflood@gmail.com>
2018-04-02 15:57:58 +0100

    Make comments more descriptive

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
- // Display the cards on the page
+ // List the card icons
 const icons = [
 	'bicycle',
 	'bicycle',
@@ -19,6 +19,7 @@ const icons = [
 ];
 const deck = document.querySelector('.deck');
 
+// Create each card element and shuffle the cards
 function makeCards() {
 	const shuffledCards = shuffle(icons);
 
@@ -29,11 +30,13 @@ function makeCards() {
 		card.innerHTML = newCard;
 		deck.appendChild(card);
 	}
-
+	// Click listener for card elements
 	deck.addEventListener('click', cardOpen);
+	// Click listener to start game timer
 	deck.addEventListener('click', timer);
 }
 
+// Display the shuffled cards on the page
 makeCards();
 
 // Shuffle function from http://stackoverflow.com/a/2450976
@@ -51,26 +54,31 @@ function shuffle(array) {
     return array;
 }
 
-// Matching cards logic
+// Display card symbol on click
 let openCards = [];
 const cards = deck.getElementsByTagName('li');
 
 function cardOpen(event) {
 	if (event.target.nodeName == 'LI') {
 		event.target.className = 'card open show';
+	 	// Add clicked cards to 'open cards' array for comparison
 	 	openCards.push(event.target.innerHTML);
 	 	if (openCards.length == 2) {
+	 		// Remove click listeners to compare 'open cards'
 	 		deck.removeEventListener('click', cardOpen);
 	 		compareCards();
 	 	}
+	 	// Update move count
 	 	moveCounter();
 	}
  }
 
+// Check for card matching
 function compareCards() {
 	openCards[0] === openCards[1] ? setTimeout(cardMatch, 500) : setTimeout(cardNotMatch, 700);
 }
 
+// Card matching 
 function cardMatch() {
 	openCards = [];
 	for (let i = 0; i < cards.length; i++) {
@@ -78,9 +86,11 @@ function cardMatch() {
 			cards[i].className = 'card match';
 		}
 	}
+	// Replace click listeners on cards
 	deck.addEventListener('click', cardOpen);
 }
 
+// Card not matching
 function cardNotMatch() {
 	openCards = [];
 	for (let i = 0; i < cards.length; i++) {
@@ -88,40 +98,48 @@ function cardNotMatch() {
 			cards[i].className = 'card';
 		}
 	}
+	// Replace click listeners on cards
 	deck.addEventListener('click', cardOpen);
 }
 
-// Move counter
+// Update move count and star rating
 const scorePanel = document.querySelector('.score-panel');
 const stars = scorePanel.getElementsByTagName('li');
 const moves = scorePanel.querySelector('.moves');
 let clickCount = 0;
 
 function moveCounter() {
+	// Increment move counter
 	clickCount++;
 	moves.innerHTML = clickCount;
+	// Set click limits for star ratings
  	if (clickCount == 15) {
  		stars[0].style.visibility = 'hidden';
  	} else if (clickCount == 25) {
  		stars[0, 1].style.visibility = 'hidden';
  	} else if (clickCount == 40) {
  		stars[0, 1, 2].style.visibility = 'hidden';
+ 		// Display game over modal
  		setTimeout(gameOver, 500);
  	}
 }
 
-// Timer
+// Game timer function based on examples from https://stackoverflow.com/questions/30747364
 const timerText = document.querySelector('.timer');
 let counter = 0;
 
 function timer(event) {
+	// Timer starts on first click only
 	if (event.target.nodeName == 'LI') {
 		deck.removeEventListener('click', timer);
 	}
+	// Increment timer every second
 	timer = setInterval(function() {
 		counter++;
+		// Calculate minutes and seconds
 		let m = ((counter / 60) | 0);
 		let s = counter % 60;
+		// Add leading zeros if minutes or seconds are < 10
 		if (m < 10) {
 			m = '0' + m;
 		}
@@ -129,14 +147,16 @@ function timer(event) {
 			s = '0' + s;
 		}
 		timerText.innerHTML = m + ':' + s;
+		// Stop timer once all cards are matched
 		if (document.getElementsByClassName('card match').length == 16) {
 			clearInterval(timer);
+			// Display game win modal
 			gameWin();
 		}
 	}, 1000);
 }
 
-// Winning modal
+// Game win modal
 const winModal = document.querySelector('.win-modal');
 const winTime = document.querySelector('.win-time');
 const winMessage = document.querySelector('.win-stats');
@@ -158,11 +178,12 @@ function gameOver() {
 	tryAgain.addEventListener('click', restartGame);
 }
 
-// Restart game
+// Reset game during game play
 const reset = document.querySelector('.restart');
 
 reset.addEventListener('click', restartGame);
 
+// Restart game 
 function restartGame() {
 	location.reload();
 }
